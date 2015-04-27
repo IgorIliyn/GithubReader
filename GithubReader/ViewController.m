7d2c8f2c1541e498ba90229d0b18ee8d752792ca
Igor Iliyn <igordevmail@gmail.com>
2015-04-27 14:50:30 +0300

    Add localisation

--- GithubReader/ViewController.m
+++ GithubReader/ViewController.m
@@ -16,6 +16,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.title = NSLocalizedString(@"Hello", nil);
     // Do any additional setup after loading the view, typically from a nib.
 }
 
